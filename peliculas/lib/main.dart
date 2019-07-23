4980e359ac11e6bf649b474cdbd987b3750f5bb2
Manuel <manuel.toro.rivas@gmail.com>
2019-07-23 22:16:46 +0200

    Modified main, added an new widget to manage cards

--- peliculas/lib/main.dart
+++ peliculas/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
 import 'package:peliculas/src/pages/home_page.dart';
  
 void main() => runApp(MyApp());
--- peliculas/lib/src/pages/home_page.dart
+++ peliculas/lib/src/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -7,9 +7,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text("Peliculas app"),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Peliculas de cine'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
       ),
+      body: Container(
+        child: Column(
+          children: <Widget>[_swiperTarjetas()],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return CardSwiper(
+      peliculas : [1,2,3,4],
     );
   }
 }
